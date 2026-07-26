97abd252983aa94a0856bc58386c2af3e787b371
policiu <1704165+policiu@users.noreply.github.com>
2026-07-26 19:38:54 -0400

    Example Starting Deck Replacer

---
+++ AlternativeStartingDecksCode/Patches/Utils/ReplaceIroncladStarterCardsExample.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.Models.Characters;
+
+namespace AlternativeStartingDecks.AlternativeStartingDecksCode.Patches.Utils;
+
+[HarmonyPatch(typeof(Ironclad), nameof(Ironclad.StartingDeck), MethodType.Getter)]
+public class ReplaceIroncladStarterCardsExample
+{
+    public static IEnumerable<CardModel> Postfix(IEnumerable<CardModel> __result)
+    {
+        return InjectDifferentCards(__result);
+    }
+
+
+    private static IEnumerable<CardModel> InjectDifferentCards(IEnumerable<CardModel> _)
+    {
+        return
+        [
+            ModelDb.Card<StrikeDefect>(),
+            ModelDb.Card<StrikeDefect>(),
+            ModelDb.Card<StrikeDefect>(),
+            ModelDb.Card<StrikeDefect>(),
+            ModelDb.Card<StrikeDefect>(),
+            ModelDb.Card<StrikeDefect>(),
+            ModelDb.Card<StrikeDefect>()
+        ];
+    }
+}
