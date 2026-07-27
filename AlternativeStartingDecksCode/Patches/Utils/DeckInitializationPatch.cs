8dc3967e6c8a0da595d50f3dcbf14b4c0da64b44
policiu <1704165+policiu@users.noreply.github.com>
2026-07-26 21:35:17 -0400

    Add some Decks

---
+++ AlternativeStartingDecksCode/Patches/Utils/DeckInitializationPatch.cs
@@ -0,0 +1,21 @@
+﻿using AlternativeStartingDecks.AlternativeStartingDecksCode.Utils;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.Models.Characters;
+using MegaCrit.Sts2.Core.Models.Relics;
+
+namespace AlternativeStartingDecks.AlternativeStartingDecksCode.Patches.Utils;
+
+[HarmonyPatch]
+internal class DeckInitializationPatch
+{
+    [HarmonyPatch(typeof(ModelDb), "InitIds")]
+    [HarmonyPrefix]
+    private static void LatePostInit()
+    {
+        StartingInventoryManager.AddNewInventoryForCharacter(nameof(Ironclad), "default",
+            new AlternativeStartingInventory([ModelDb.Card<DefendNecrobinder>()], potions: [],
+                relics: [ModelDb.Relic<Pear>()]));
+    }
+}
