c56505520be2b51b88cc68eed3431aabd666b3e3
policiu <1704165+policiu@users.noreply.github.com>
2026-07-28 11:20:34 -0400

    Add Deck Info Panel Placeholder
    Try to get Hot Reload working

---
+++ AlternativeStartingDecksCode/Patches/Ui/DeckSelectorPatch.cs
@@ -0,0 +1,117 @@
+﻿using AlternativeStartingDecks.AlternativeStartingDecksCode.Scenes.Screens;
+using BaseLib.Config;
+using Godot;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Nodes.Screens.CharacterSelect;
+using Timer = Godot.Timer;
+
+namespace AlternativeStartingDecks.AlternativeStartingDecksCode.Patches.Ui;
+
+// Don't Look, def won't crash... promise?
+internal static class SharedUi
+{
+    public static Button? Button;
+}
+
+public class DeckSelectorPatch
+{
+    [HarmonyPatch(typeof(NCharacterSelectScreen), nameof(NCharacterSelectScreen._Ready))]
+    public static class ReadyPatch
+    {
+        public static void Postfix(NCharacterSelectScreen __instance)
+        {
+            try
+            {
+                InjectDeckSelectorUi(__instance);
+            }
+            catch (Exception e)
+            {
+                ModConfig.ModConfigLogger.Error(
+                    "AlternativeStartingDecks was unable to add the Deck Selection to the Character SelectScreen." +
+                    "This is likely either due to a recent game update, or mod incompatibility." +
+                    $"{e.Message}");
+            }
+        }
+
+        private static void InjectDeckSelectorUi(NCharacterSelectScreen screen)
+        {
+            var button = new Button();
+            var infoPanelOg = screen.GetNodeOrNull<Control>("InfoPanel");
+
+            void Clicked()
+            {
+                button.Text = button.GetGlobalMousePosition() + " " +
+                              ((Control)button.GetParent()).GetLocalMousePosition() + '\n';
+                button.Text += (screen.GetViewport().GuiGetHoveredControl()?.Name ?? "No Element") + "\n";
+                button.Text += screen.GetViewport().GuiGetHoveredControl()?.GetParentControl()?.Name ?? "No Element";
+            }
+
+
+            button.Text = "Add Character Select";
+            button.CustomMinimumSize = new Vector2(150, 50);
+            button.Position = new Vector2(-940, -1000);
+            screen.GetNodeOrNull("CharSelectButtons/ButtonContainer")?.AddSibling(button);
+            var a = screen.GetNodeOrNull<Button>("CharSelectButtons");
+            button.MouseEntered += Clicked;
+            SharedUi.Button = button;
+
+            // Add Timer
+            var myTimer = new Timer();
+            myTimer.WaitTime = 1;
+            myTimer.SetOneShot(false);
+            myTimer.Timeout += Clicked;
+            button.AddSibling(myTimer);
+            myTimer.Start();
+
+            // Anyways :)
+            var next = DeckInfoPanel.LoadScene();
+            infoPanelOg.AddSibling(next);
+        }
+    }
+
+    [HarmonyPatch(typeof(NCharacterSelectScreen), nameof(NCharacterSelectScreen.SelectCharacter))]
+    public static class SelectCharacterPatch
+    {
+        // Oh no
+        private static Tween? _deckInfoTween;
+        private static Vector2 _deckInfoPosition;
+
+        [HarmonyPostfix]
+        public static void Postfix(NCharacterSelectScreen __instance)
+        {
+            try
+            {
+                InjectSelectCharacter(__instance);
+            }
+            catch (Exception e)
+            {
+                ModConfig.ModConfigLogger.Warn("[ASD] Unable to inject the Character SelectScreen. " + "\n" +
+                                               e.Message);
+            }
+        }
+
+        private static void InjectSelectCharacter(NCharacterSelectScreen screen)
+        {
+            var deckInfoPanel = screen.GetNodeOrNull<Control>("DeckInfoPanel");
+            if (deckInfoPanel == null) return;
+            if (_deckInfoTween != null) deckInfoPanel.Position = _deckInfoPosition;
+            _deckInfoPosition = deckInfoPanel.Position;
+            _deckInfoTween?.Kill();
+            _deckInfoTween = screen.CreateTween().SetParallel();
+
+            _deckInfoTween.TweenProperty(deckInfoPanel, (NodePath)"position", deckInfoPanel.Position, 0.5)
+                .SetEase(Tween.EaseType.Out).SetTrans(Tween.TransitionType.Expo)
+                .From(deckInfoPanel.Position + new Vector2(300f, 0.0f));
+        }
+    }
+}
+
+[HarmonyPatch(nameof(NCharacterSelectButton), nameof(NCharacterSelectButton.Select))]
+public static class NCharacterSelectButton_Select
+{
+    public static void Postfix(NCharacterSelectButton __instance)
+    {
+        var button = SharedUi.Button;
+        if (button != null) button.Text = __instance._character.CharacterSelectTitle;
+    }
+}
---
+++ AlternativeStartingDecksCode/Scenes/Screens/DeckInfoPanel.cs
@@ -0,0 +1,35 @@
+﻿using Godot;
+using MegaCrit.Sts2.Core.Assets;
+
+namespace AlternativeStartingDecks.AlternativeStartingDecksCode.Scenes.Screens;
+
+public static class DeckInfoPanel
+{
+    public static Node? LoadScene(string name = "DeckInfoPanel")
+    {
+        var result = PreloadManager.Cache.GetScene("res://AlternativeStartingDecks/scenes/screens/deck_info_panel.tscn")
+            .Instantiate();
+        if (result == null) return null;
+        // Load Dependencies
+        var charSelectPng = PreloadManager.Cache.GetTexture2D("res://images/ui/fuzzy_nine_patch_char_select.png");
+        var hpPng = PreloadManager.Cache.GetTexture2D(
+            "res://images/atlases/ui_atlas.sprites/top_bar/top_bar_heart.tres");
+        var goldPng =
+            PreloadManager.Cache.GetTexture2D("res://images/atlases/ui_atlas.sprites/top_bar/top_bar_gold.tres");
+        var placeholderPng = PreloadManager.Cache.GetTexture2D("res://images/debug/placeholder_64.png");
+
+        var megaLabelScript = PreloadManager.Cache.GetAsset<Script>("res://addons/mega_text/MegaLabel.cs");
+        var megaLabelRichScript = PreloadManager.Cache.GetAsset<Script>("res://addons/mega_text/MegaRichTextLabel.cs");
+
+        // Apply Dependencies
+        result.GetNode<NinePatchRect>("NinePatchRect").Texture = charSelectPng;
+        result.GetNode<TextureRect>("VBoxContainer/HpGoldSpacer/HpGold/Hp/Icon").Texture = hpPng;
+        result.GetNode<TextureRect>("VBoxContainer/HpGoldSpacer/HpGold/Gold/Icon").Texture = goldPng;
+        result.GetNode<TextureRect>("VBoxContainer/Relic/Icon").Texture = placeholderPng;
+        result.GetNode<RichTextLabel>("VBoxContainer/Relic/Description").SetScript(megaLabelRichScript);
+
+        result.Name = name;
+
+        return result;
+    }
+}
